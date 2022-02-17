18c7e3ab2a300e50822b0efa7c333c0321b89d77
kchaure10 <kevin.chaure10@hotmail.com>
2022-02-17 16:13:27 -0300

    Se agrega clase armamento

--- src/logica/armamento.js
+++ src/logica/armamento.js
@@ -1,13 +1,39 @@
 export class Armamento {
-    constructor(cod,nombre,calibre,descripcion,municion,danio){
 
-        // Atributos propios
-        this.cod = cod;
-        this.nombre = nombre;
-        this.calibre = calibre;
-        this.descripcion = descripcion;
-        this.municion = municion;
-        this.danio = danio;
+    constructor(tipoarmamanto,cod,nombre,calibre,descripcion,municion,danio){
+        if(tipoarmamento === "CAÑON105"){
+            this.cod = 1;
+            this.nombre = "CAÑON 105 MM";
+            this.calibre = "105 MM";
+            this.descripcion = "CAÑON DE 105 MM";
+            this.municion = 20;
+            this.danio=1;
+        }else{
+            if (tipoarmamento === "TORPEDO"){
+                this.cod = 2;
+                this.nombre = "TORPEDO";
+                this.calibre = "533 MM";
+                this.descripcion = "TORPEDO";
+                this.municion = 16;
+                this.danio = 2;
+            }else{
+                if(tipoarmamento === "CAÑON127"){
+                    this.cod=3;
+                    this.nombre="CAÑON 127 MM";
+                    this.calibre="127 MM";
+                    this.descripcion = "CAÑON DE 127 MM";
+                    this.municion = 30;
+                    this.danio = 1;
+                }else{
+                    this.cod=4;
+                    this.nombre="CARGA DE PROFUNDIDAD";
+                    this.calibre="CAÑON 127 MM";
+                    this.descripcion = "CARGA DE PROFUNDIDAD";
+                    this.municion = 10;
+                    this.danio = 3;
+                };
+            };
+        }; 
     };
 
     // Metodos: se definen los metodos de manera interna a la funcion para no generar conflictos con los nombres de otros metodos globalmente
