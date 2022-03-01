0a1bb919abd8ea2453e1853fc4cee04fac3336a1
Jona Diaz <jonagdd@gmail.com>
2022-03-01 14:41:51 -0300

    Se agrega sala espera para partida en curso

--- src/def.js
+++ src/def.js
@@ -50,6 +50,7 @@ export const DEF = {
         LOADS: "load.png",
         TXTESP: "txtespera.png",
         TXTESP2: "txtespera2.png",
+        TXTESP3: "textespera3.png",
         INITPART: "iniciarpart.png",
         CARGARPART: "cargarpart.png",
         CONTROLS: "controls.png",
