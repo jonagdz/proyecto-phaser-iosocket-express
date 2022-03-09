8686d7601b43db93b0f796b30427d5edf5044c26
IvanL17 <ivanlarrosa@gmail.com>
2022-03-08 21:17:54 -0300

    h

--- src/escenas/gameOver.js
+++ src/escenas/gameOver.js
@@ -19,7 +19,7 @@ export class gameOver extends Phaser.Scene{
     const screenCenterY = self.cameras.main.worldView.y + self.cameras.main.height / 2;
 
     self.add.image(0, 0, DEF.IMAGENES.FONDO).setOrigin(0).setScrollFactor(1);
-    self.sonidoGameOver = this.sound.add(DEF.AUDIO.ENDGAME,{volume: 0.4, loop: true});
+    self.sonidoGameOver = self.sound.add(DEF.AUDIO.ENDGAME,{volume: 0.4, loop: true});
     self.sonidoGameOver.play();
     
     self.home= self.add.sprite(screenCenterX, 650, 'homes').setOrigin(0).setScrollFactor(1).setDepth(2).setInteractive().on('pointerdown', () => volverMenu());
